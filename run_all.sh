879d6610d9e4191b6643160f8793f5ea37dd2817
Shubham <shubhamgupta5893@gmail.com>
2023-05-22 15:48:58 +0530

    removed unnecessary files

--- run_all.sh
+++
@@ -1,6 +0,0 @@
-sh repeat_code_bs.sh Computer 0 2
-sh repeat_code_bs.sh Computer 0.5 2
-sh repeat_code_bs.sh Computer 0.99 2
-sh repeat_code_bs.sh Computer 0.999 2
-sh repeat_code_bs.sh Computer 0.9999 2
-sh repeat_code_bs.sh Computer 0.99999 2
